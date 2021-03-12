17299743860acdac22712822a77cce5e3e64399f
Kaihack <prostor.za.rad@gmail.com>
2021-03-12 10:32:26 +0100

    Added index.js - Declaring string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    
+    // Only change code below this line
+    
+    var myFirstName = "George";
+    var myLastName = "Freedman";
+    var myHomeTown = "Mineapolis";
+    
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };   
+}
+    
+    console.log(main()); 
+    module.exports = main;
